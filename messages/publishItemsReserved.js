df970b2cf5e4dcda4e6eaa18f5e3ef051db36f59
Dani025a <dani025a@edu.easj.dk>
2024-05-24 10:47:14 +0200

    small changes

--- messages/publishItemsReserved.js
+++ messages/publishItemsReserved.js
@@ -3,7 +3,7 @@ import { connectToOrderFanoutExchange } from "./connectToExchanges.js";
 const { channel, exchangeName } = await connectToOrderFanoutExchange();
 
 async function publishItemsReservedEvent(message) {
-    channel.publish(exchangeName, 'items_reserved_failed', Buffer.from(JSON.stringify(message)));
+    channel.publish(exchangeName, 'items reserved failed', Buffer.from(JSON.stringify(message)));
 }
 
 export {
