3914cd197f0b50276e0e26004ac43d1be1ea8d03
Dani025a <dani025a@edu.easj.dk>
2024-05-24 10:52:06 +0200

    Changed to correct publisher

--- messages/publishItemsReservedFailed.js
+++ messages/publishItemsReservedFailed.js
@@ -3,7 +3,7 @@ import { connectToOrderDirectExchange } from "./connectToExchanges.js";
 const { channel, exchangeName } = await connectToOrderDirectExchange();
 
 async function publishItemsReservedFailedEvent(message) {
-    channel.publish(exchangeName, '', Buffer.from(JSON.stringify(message)));
+    channel.publish(exchangeName, 'items reserved failed', Buffer.from(JSON.stringify(message)));
 }
 
 export {
